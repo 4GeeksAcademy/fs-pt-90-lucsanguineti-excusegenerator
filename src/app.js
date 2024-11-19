657d2685ca053f7b267d7ccac89dde76c35e3242
Luc <lucsanguineti@protonmail.com>
2024-11-19 18:37:22 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandomElement = listElements => {
+    let element = listElements[Math.floor(Math.random() * listElements.length)];
+    return element;
+  };
+
+  let excuseGen = document.querySelector("#excuse");
+  console.log(excuseGen);
+  excuseGen.innerHTML =
+    getRandomElement(who) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what) +
+    " " +
+    getRandomElement(when);
 };
